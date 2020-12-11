e1dc50d405f799de7fa4042e79cf414a27a74f42
Larissa Alves <1903larissaalves@gmail.com>
2020-12-11 10:42:47 -0300

    Validando informaçãoes antes de calcular e mostrando resultado na linha da tabela

--- js/index.js
+++ js/index.js
@@ -8,18 +8,30 @@ titulo.textContent = "Aparecida Nutricionista";
 //Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
 //Obesidade, quando o resultado é maior que 30 kg/m2.
 
-var pesoPaciente01 = document.querySelector(".info-peso")
+var paciente01 = document.querySelector("#paciente01");
+
+var pesoPaciente01 = paciente01.querySelector(".info-peso")
 var peso = pesoPaciente01.textContent;
-console.log(pesoPaciente01);
-console.log("O peso do paciente é " + peso);
 
-var alturaPaciente01 = document.querySelector(".info-altura");
+var alturaPaciente01 = paciente01.querySelector(".info-altura");
 var altura = alturaPaciente01.textContent;
-console.log(alturaPaciente01);
-console.log("A altura é " + altura);
 
-var imc;
-imc = peso / (altura * altura);
-console.log("O imc do paciente 01 é de " + imc);
+var pesoValido = true;
+var alturavalida = true;
 
+var infoImc = document.querySelector(".info-imc");
 
+if (peso <= 0 || peso >=800) {
+    pesoValido = false;
+    infoImc.textContent = "Peso Inválido!";
+}
+
+if (altura <= 0 || altura >=3.00) {
+    alturavalida = false;
+    infoImc.textContent = "Altura Inválido!";
+}
+
+if (pesoValido && alturavalida) {
+    var imc = peso / (altura * altura);
+    infoImc.textContent = imc;
+}
