5916ae1bdb32203631d7e922e13d1f792dce45d3
Larissa Alves <1903larissaalves@gmail.com>
2020-12-11 16:21:25 -0300

    Organizacao do codigo

--- js/index.js
+++ js/index.js
@@ -37,42 +37,15 @@ for (var i = 0; i < pacientes.length; i++) {
         infoImc.textContent = "Altura Inválido!";
         paciente.classList.add("paciente-invalido");
     }
-
+    
     if (pesoValido && alturavalida) {
-        var imc = peso / (altura * altura);
-        infoImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso, altura)
+        infoImc.textContent = imc;
     }
 }
 
-var adicionarPaciente = document.querySelector("#adicionar-paciente");
-adicionarPaciente.addEventListener("click", function(e) {
-    e.preventDefault();
-
-    var form = document.querySelector("#form-adicionar");
-
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var novoPaciente = document.querySelector("#tabela-pacientes");
-    novoPaciente.appendChild(pacienteTr);
-
-});
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
